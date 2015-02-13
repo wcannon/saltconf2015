7bbbed1c7378490dab04843bc7ee7cea05407b6f
William Cannon <william@hospitalitypulse.com>
2015-02-13 15:53:04 -0600

    updating bootstrap for new paths in s3

--- s3_bucket_contents/aws_scripts/saltmaster_bootstrap.sh
+++ s3_bucket_contents/aws_scripts/saltmaster_bootstrap.sh
@@ -29,17 +29,15 @@ apt-get install -y salt-doc
 apt-get install -y salt-master
 
 # download master config file
-aws s3 cp s3://saltconf2015bucket/masters_master_config /etc/salt/master
+aws s3 cp s3://saltconf2015bucket/master/saltmaster_config  /etc/salt/master
 
 ################################################################################
 ####### Need to git clone the repo locally so that a highstate can run #########
 ################################################################################
 # git clone into local dir  /root/saltconf2015
-git clone https://github.com/wcannon/saltconf2015.git
+git clone https://github.com/wcannon/saltconf2015.git  /root/
 
 # create symlinks
-#  pillar  reactor  README.md  s3_bucket_contents  salt
-#ln -s /home/wcannon/saltconf2015/saltconf2015/salt /tmp/salt
 ln -s /root/saltconf2015/pillar /srv/pillar
 ln -s /root/saltconf2015/reactor /srv/reactor
 ln -s /root/saltconf2015/salt /srv/salt
@@ -47,18 +45,20 @@ ln -s /root/saltconf2015/salt /srv/salt
 # restart salt-master to pick up any master config file changes
 service salt-master restart
 
+###################################################################
 ## If master encryption key already in S3 bucket, replace local key with it and restart salt-master
 ## If not, push the new master encryption key up to the S3 bucket
+###################################################################
 
 # install salt-minion
 apt-get install -y salt-minion
 
 # download minion config file -- contains a 'master: 127.0.0.1 record' and other
 # necessary info e.g. file_roots, custom modules and etc
-aws s3 cp s3://saltconf2015bucket/masters_minion_config /etc/salt/minion
+aws s3 cp s3://saltconf2015bucket/master/saltmaster_minion_config  /etc/salt/minion
 
 # download grains file - contains info about self (e.g. administration role)
-aws s3 cp s3://saltconf2015bucket/saltmaster-grains /etc/salt/grains
+aws s3 cp s3://saltconf2015bucket/master/saltmaster-grains  /etc/salt/grains
 
 service salt-minion restart
 
