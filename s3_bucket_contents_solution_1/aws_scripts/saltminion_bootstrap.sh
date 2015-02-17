5d11251e1ea81cf7cc3ff5ac933877d59604fda3
William Cannon <william@hospitalitypulse.com>
2015-02-17 14:57:42 -0600

    updating master to autoaccept keys, updating cf template to create autoscaling group for web servers, updated saltminion bootstrap to reflect new bucket names, updated minion config to use single hardcoded dns cname for saltmaster

--- s3_bucket_contents_solution_1/aws_scripts/saltminion_bootstrap.sh
+++ s3_bucket_contents_solution_1/aws_scripts/saltminion_bootstrap.sh
@@ -9,13 +9,14 @@ apt-get install -y salt-minion
 
 # download minion config file -- contains a 'master: 127.0.0.1 record' and other
 # necessary info e.g. file_roots, custom modules and etc
-aws s3 cp s3://saltconf2015bucket/minions_config /etc/salt/minion
+aws s3 cp s3://saltconf2015-solution-1/minion_config /etc/salt/minion
 
 # download grains file - contains info about self (e.g. administration role)
-aws s3 cp s3://saltconf2015bucket/webserver-grains /etc/salt/grains
+aws s3 cp s3://saltconf2015-solution-1/webserver-grains /etc/salt/grains
 
 service salt-minion restart
 
 # the plan is for the minion key to be sent to the master
-# a reactor on the master either rejects the key and discards it
-# or accepts the key and instructs the minion to run a highstate
+# the salt-master auto accepts the key
+# a reactor on the master instructs the minion to run a highstate
+# if a highstate has not been run previously (e.g. previous saltmaster)
