9a9002fdc3300763b9c07299ea0ac2b6c9e30cb4
root <root@ip-10-0-0-249.ec2.internal>
2015-02-20 17:14:19 +0000

    initial commit of wrapper, updates to msg, sqs, helper script to populate queue for testing

---
+++ salt/keymanager/populate_queue.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import boto.sqs
+import boto.sqs.message
+
+region = 'us-east-1'
+queue = 'sm1'
+
+
+msg1 = open("instance_launch_msg.txt", "r").read()
+msg2 = open("instance_termination_msg.txt", "r").read()
+
+conn = boto.sqs.connect_to_region(region)
+myq = conn.get_queue(queue)
+my_messages = [msg1,msg1]
+for m in my_messages:
+  m =  boto.sqs.message.Message()
+  m.set_body(msg1)
+  myq.write(m)
+
--- salt/keymanager/sqs.py
+++ salt/keymanager/sqs.py
@@ -2,34 +2,62 @@
 import sys
 import boto.sqs
 
-def get_all(queues):
-  return conn.get_all_queues()
-
 def get_queue_length(conn, queue):
-  myq = conn.get_queue('sm1')
+  try:
+    myq = conn.get_queue('sm1')
+  except:
+    raise
   return  myq.count()
   
 def get_a_message(conn, queue):
-  myq = conn.get_queue('sm1')
-  message = myq.read()
+  try:
+    myq = conn.get_queue('sm1')
+    message = myq.read()
+  except:
+    raise
   return message
 
 def print_message(message):
-  print message.get_body()
+  try:
+    print message.get_body()
+  except:
+    raise
   return
 
 def delete_a_message(conn, queue, message):
-  myq = conn.get_queue('sm1')
-  myq.delete_message(message)
+  try:
+    myq = conn.get_queue('sm1')
+    myq.delete_message(message)
+  except:
+    raise
   return 
 
+def main(region, queue_name):
+  '''Retrieve all messages from a specified queue, and return them as a list'''
+  message_list = []
+  try:
+    conn = boto.sqs.connect_to_region(region)
+    num_messages = get_queue_length(conn, queue_name)
+    if num_messages > 0:
+      while num_messages > 0:
+        message = get_a_message(conn, queue_name)
+        message_list.append(message)
+        delete_a_message(conn, queue_name, message)
+        num_messages -= 1
+  except Exception, e:
+    print "Exception: %s" % e
+    raise
+  return message_list    
+
+
 if __name__ == "__main__":
+  # e.g. ./msg.py us-east-1 sm1
   region = sys.argv[1]
-  queue = sys.argv[2]
-  conn = boto.sqs.connect_to_region(region)
-  message1 = get_a_message(conn, queue)
-  print_message(message1)
-  print "queue length: %s" % get_queue_length(conn, queue)
-  delete_a_message(conn, queue, message1)
-  print "queue length is now: %s" % get_queue_length(conn, queue)
+  queue_name = sys.argv[2]
+  try:
+    main(region, queue_name)
+  except:
+    print "Error in sqs message retrieval"
+    raise
+
 
