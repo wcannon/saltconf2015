ebeff9326addd021d4a07020262235b26d1936bc
root <root@ip-10-0-10-41.ec2.internal>
2015-02-22 02:50:46 +0000

    added a logic change on highstate check, and lots of logging

--- runners/highstate_manager.py
+++ runners/highstate_manager.py
@@ -14,27 +14,42 @@ Options:
 from docopt import docopt
 import salt.client
 import socket
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def check_if_first_highstate_ever(minion_id):
   '''Check if a highstate has ever been run on the minion'''
   # initial highstate creates a file  --> /root/most_recent_salt_highstate_run.txt
+  log.info('FUNCTION: check_if_first_highstate_ever')
   client = salt.client.LocalClient()
   # file.path_exists_glob /root/most_recent_salt_highstate_run.txt
   mypath = ["/root/most_recent_salt_highstate_run.txt"]
-  results_by_minion = client.cmd(tgt=minion_id, fun='file.path_exists_glob', arg=mypath)   # sync call
-  return results_by_minion[minion_id]
+  results_by_minion = client.cmd(tgt=[minion_id], fun='file.path_exists_glob', arg=mypath, expr_form='list')   # sync call
+  log.info("results_by_minion: %s" % results_by_minion)
+  
+  if results_by_minion.get(minion_id, False) == False or len(results_by_minion) < 1:
+    log.info("Highstate was not run before")
+    return False
+  else:
+    log.info("Highstate WAS run before")
+    return results_by_minion[minion_id]
 
 def check_if_highstate_running(minion_id):
   '''Check if a highstate is currently running on a minion'''
+  log.info('FUNCTION: check_if_highstate_running')
   client = salt.client.LocalClient()
   #  salt ip-10-0-0-113.ec2.internal saltutil.is_running state.highstate
   running_function = ["state.highstate"]
   results_by_minion = client.cmd(tgt=minion_id, fun='saltutil.is_running', arg=running_function)   # sync call
   minion_results = results_by_minion.get(minion_id, [])
+  log.info("minion_results: %s" % minion_results)
   if not minion_results:
+    log.info("highstate is not currently running")
     return False
   else:
+    log.info("highstate IS currently running")
     return True
 
 def write_highstate_runner_claim(minion_id):
@@ -46,7 +61,6 @@ def write_highstate_runner_claim(minion_id):
   hostname = socket.gethostname()
   results = client.cmd(tgt=minion_id, fun='file.touch', arg=[path])   # sync call
   results_by_minion = client.cmd(tgt=minion_id, fun='file.append', arg=[path, hostname])   # sync call
-  print results_by_minion
   return
 
 def should_run_highstate(minion_id):
@@ -74,34 +88,39 @@ def call_highstate(minion_id):
 def main(minion_id):
   '''Wrap all this goodness together coherently'''
   # Return True if this saltmaster runs highstate, False otherwise
-  highstate_already_run = ever_run = check_if_first_highstate_ever(minion_id)
+  highstate_already_run =  check_if_first_highstate_ever(minion_id)
   if highstate_already_run:
-    #print "Highstate run at least once before.  Exiting."
+    print "Highstate run at least once before.  Exiting."
+    log.info("Highstate run at least once before. Exiting")
     return
   highstate_running_now = check_if_highstate_running(minion_id)
   if highstate_running_now:
-    #print "Highstate is already running.  Exiting."
+    print "Highstate is already running.  Exiting."
+    log.info("Highstate is already running. Exiting")
     return
   # Looks like we might need to run a highstate
 
   # Attempt to claim the right to run highstate on minion
-  #print "Attempting to claim the highstate run"
+  print "Attempting to claim the highstate run"
+  log.info("Attempting to claim the highstate run")
   write_highstate_runner_claim(minion_id) 
 
   winner = should_run_highstate(minion_id)
   if winner:
-    #print "I'm the winner"
+    print "I'm the winner"
+    log.info("I'm the winner")
     call_highstate(minion_id)
     return True
   else:   
-    #print "Another salt master will run highstate.  Exiting."
+    print "Another salt master will run highstate.  Exiting."
+    log.inf("Another salt master will run highstate.  Exiting.")
     return False
 
 def manage(miniondata="no miniondata passed"):
   result = False
-  f = open("/tmp/highstate-data", 'w')
-  f.write(str(miniondata))
-  f.close()
+  #f = open("/tmp/highstate-data", 'w')
+  #f.write(str(miniondata))
+  #f.write("\n")
   minion_id = miniondata['id']
   act =  miniondata['act']
   if act == 'accept':
