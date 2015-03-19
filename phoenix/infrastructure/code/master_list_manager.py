d3c9810fe39909411b7366d0031efbca46c14281
root <root@ip-10-0-0-72.ec2.internal>
2015-03-19 18:29:45 +0000

    fixing up unicode vs. str representation in lists

--- phoenix/infrastructure/code/master_list_manager.py
+++ phoenix/infrastructure/code/master_list_manager.py
@@ -6,17 +6,17 @@ import helper
 import subprocess
 import ddb
 
-POLL_CYCLE = 60 # seconds to sleep between polling dynamodb for list of active salt masters
+POLL_CYCLE = 10 # seconds to sleep between polling dynamodb for list of active salt masters
 CONFIG_FILE = "/etc/salt/minion"
 TABLE_NAME = "masters"
 SVC_NAME = "salt-minion"
 
 def get_current_salt_masters(config_file=CONFIG_FILE):
   '''retrieve and return the list of current salt masters out of the minion config file'''
-  masters = []
+  masters= []
   try:
     info_yaml = yaml.load(open(config_file, "r").read())
-    masters = info_yaml.get('master', None)
+    masters = info_yaml.get('master', [])
   except Exception, e:
     raise
   return masters
@@ -29,7 +29,7 @@ def get_active_salt_masters():
     masters = a.scan()
     for m in masters:
       if m.active == True:
-         master_ipaddrs.append(m.ipaddr)
+         master_ipaddrs.append(str(m.ipaddr))
   except Exception, e:
     raise
   return master_ipaddrs
@@ -38,9 +38,12 @@ def update_config(masters_list, config_file=CONFIG_FILE):
   '''update CONFIG_FILE with active salt masters if needed'''
   try:
     info_yaml = yaml.load(open(config_file, "r").read())
+    print 
+    print "info yaml contains: %s" % info_yaml
+    print
     info_yaml['master'] = masters_list
-    stream = file(config_file, 'w')
-    yaml.dump(info_yaml, stream)
+    with open(config_file, 'w') as yaml_file:
+      yaml_file.write( yaml.dump (info_yaml, default_flow_style=False))
   except Exception, e:
     raise
   return
@@ -59,24 +62,30 @@ def restart_local_minion():
 def main():
   try:
     region = helper.get_region()
-    print "zone is: %s" % region
     ddb.set_region(region)
-    local_sm_list = get_current_salt_masters()
-    print "local_sm_list is: %s" % local_sm_list
   except Exception, e:
-    print "Unable to get current salt masters from minion config"
-    print e
-    raise
-  # need to set the dynamodb region to connect to here --> region 
+    print "Exception: %s" % e
+    sys.exit(1) 
+  while True:
+    try:
+      print "zone is: %s" % region
+      local_sm_list = get_current_salt_masters()
+      print "local_sm_list is: %s" % local_sm_list
+    except Exception, e:
+      print "Unable to get current salt masters from minion config"
+      print e
+      raise
   
-  dynamodb_sm_list = get_active_salt_masters()
-  print "dynamodb_sm_list is: %s" % dynamodb_sm_list
-  if local_sm_list != dynamodb_sm_list: # are lists unequal
-    print "lists don't match...updating config and restarting minion"
-    update_config(masters_list=dynamodb_sm_list)
-    restart_local_minion()
-  print "sleeping for %s seconds" % POLL_CYCLE
-  time.sleep(POLL_CYCLE)
+    dynamodb_sm_list = get_active_salt_masters()
+    print "dynamodb_sm_list is: %s" % dynamodb_sm_list
+    if set(local_sm_list) != set(dynamodb_sm_list): # are lists unequal
+      print "lists don't match...updating config and restarting minion"
+      update_config(masters_list=dynamodb_sm_list)
+      restart_local_minion()
+    else:
+      print "lists match, sleeping"
+    print "sleeping for %s seconds" % POLL_CYCLE
+    time.sleep(POLL_CYCLE)
 
 if __name__ == "__main__":
   try:
