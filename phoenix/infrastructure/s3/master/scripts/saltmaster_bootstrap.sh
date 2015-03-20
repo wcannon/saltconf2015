39133731a94893fc8afcbf3537d3b024f54ffbd7
William Cannon <william@hospitalitypulse.com>
2015-03-20 11:36:57 -0500

    adding in local salt highstate

--- phoenix/infrastructure/s3/master/scripts/saltmaster_bootstrap.sh
+++ phoenix/infrastructure/s3/master/scripts/saltmaster_bootstrap.sh
@@ -33,41 +33,37 @@ echo "queuename_master: $queuename_master" >> /etc/salt/ha/ha-config
 
 # Check to see if our saltmaster.pem file exists by doing a simple ls on the expected location
 # If not, we are the first saltmster, and will push our keys into the bucket
-info=`aws s3 ls s3://saltconf2015-solution-3/master/master.pem 2>/dev/null`
+info=`aws s3 ls s3://$bucketname/master/master.pem 2>/dev/null`
 
 if [ $? -eq 0 ]
 then
   ## if the key exists, download it and restart salt-master service
   #echo "found key in s3, downloading and restarting salt-master"
   service salt-master stop
-  aws s3 cp s3://saltconf2015-solution-3/master/master.pem /etc/salt/pki/master/master.pem
-  aws s3 cp s3://saltconf2015-solution-3/master/master.pub /etc/salt/pki/master/master.pub
+  aws s3 cp s3://$bucketname/master/master.pem /etc/salt/pki/master/master.pem
+  aws s3 cp s3://$bucketname/master/master.pub /etc/salt/pki/master/master.pub
   service salt-master start 
 else 
   ## key does not exist, upload our key as we are the first salt-master
   #echo "key not found in s3, uploading master key to s3"
-  aws s3 cp /etc/salt/pki/master/master.pem s3://saltconf2015-solution-3/master/master.pem
-  aws s3 cp /etc/salt/pki/master/master.pub s3://saltconf2015-solution-3/master/master.pub
+  aws s3 cp /etc/salt/pki/master/master.pem s3://$bucketname/master/master.pem
+  aws s3 cp /etc/salt/pki/master/master.pub s3://$bucketname/master/master.pub
 fi
 
 # Need to git clone the repo locally so that a highstate can run on the salt-master
 git clone https://github.com/wcannon/saltconf2015.git  /root/saltconf2015
 
 # Copy in master config
-aws s3 cp s3://saltconf2015-solution-3/master/config/saltmaster_config /etc/salt/master
+aws s3 cp s3://$bucketname/master/config/saltmaster_config /etc/salt/master
 
 # create symlinks
-ln -s /root/saltconf2015/solution-three/pillar /srv/pillar
-ln -s /root/saltconf2015/solution-three/salt /srv/salt
-ln -s /root/saltconf2015/solution-three/salt/reactor /srv/reactor
-ln -s /root/saltconf2015/solution-three/salt/runners /srv/salt/runners
+ln -s /root/saltconf2015/phoenix/pillar /srv/pillar
+ln -s /root/saltconf2015/phoenix/salt /srv/salt
+ln -s /root/saltconf2015/phoenix/salt/reactor /srv/reactor
+ln -s /root/saltconf2015/phoenix/salt/runners /srv/salt/runners
 
 # restart salt-master to pick up any master config file changes
 service salt-master restart
-
-# Download instance_manager.py
-# Download instace_manager.conf upstart script
-# start the instance_manager service
 
 ## Instead of installing salt-minion & etc, we download the minion bootstrap
 ## and run it passing in params just like any minion receives
@@ -75,3 +71,20 @@ aws s3 cp s3://$bucketname/minion/scripts/saltminion_bootstrap.py .
 chmod +x saltminion_bootstrap.py
 /usr/bin/python ./saltminion_bootstrap.py $bucketname $grainsfile
 
+# get our instanceid
+$myid='curl http://169.254.169.254/latest/meta-data/instance-id/'
+
+# accept our minion key
+salt-key -y -a $myid
+
+# run local highstate
+salt-call --local state.highstate
+
+# Download instance_manager.py
+mkdir -p /usr/local/bin/phoenix
+aws s3 cp s3://$bucketname/minion/scripts/instance_manager.py /usr/local/bin/phoenix
+aws s3 cp s3://$bucketname/minion/scripts/instance_manager.conf /etc/init/instance_manager.conf
+chmod +x /usr/local/bin/phoenix/instance_manager.py
+
+# start the instance_manager service
+service instance_manager start
