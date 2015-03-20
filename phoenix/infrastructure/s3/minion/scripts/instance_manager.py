193665fb3a34d1ab9b7bf94936b20ed9eb20bb7c
root <root@ip-10-0-0-41.ec2.internal>
2015-03-20 19:58:33 +0000

    incremental...still working - saving place

--- phoenix/infrastructure/s3/minion/scripts/instance_manager.py
+++ phoenix/infrastructure/s3/minion/scripts/instance_manager.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 import sys
 import ddb
+import msg
+import sqs
+import key_deleter
 import time
 import helper
 
 POLL_CYCLE = 60 # seconds to sleep between polling dynamodb for list of active salt masters
 MASTERS_TABLE_NAME = "masters"
 MINIONS_TABLE_NAME = "minions"
+MASTER_QUEUE = "SqsMaster"
+MINION_QUEUE = "SqsMinion"
 
 
 def create_master(instanceid, ipaddr, active):
@@ -77,14 +82,37 @@ def main():
     region = helper.get_region()
     ipaddr = helper_get_private_ip()
     ddb.set_region(region)
+    ## set my ip address in the masters table
   except Exception, e:
     print "Unable to get necessary info, exiting: %s" % e
     sys.exit(1)
   
   while True:
-    ## poll masters sqs queue, update masters table
+    ## poll masters sqs queue, update masters table, and minions table
+    try:
+      sqs_conn = sqs.get_connection(region)
+      num_sqs_messages = sqs.get_queue_length(sqs_conn, MASTER_QUEUE)
+      for m in range(num_sqs_messages):
+        message = sqs.get_a_message(sqs_conn, queue)
+        # if launch, add to masters, add to minions
+        # if terminate, update masters, update minions
+    except Exception, e:
+      print "Problem with master queue management: %s" % e
+      raise
+
     ## poll minions sqs queue, update minions table
-    ## get a list of all minions which are terminated, and remove keys from self salt.Key 
+    try:
+      sqs_conn = sqs.get_connection(region)
+      num_sqs_messages = sqs.get_queue_length(sqs_conn, MINION_QUEUE)
+      for m in range(num_sqs_messages):
+        message = sqs.get_a_message(sqs_conn, queue)
+        # if launch, add to minions
+        # if terminate, update minions
+    except Exception, e:
+      print "Problem with minion queue management: %s" % e
+      raise
+    ## get a list of all minions which are terminated
+    ## and remove keys from self salt.Key 
     time.sleep(POLL_CYCLE)
 
 '''
