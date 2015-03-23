c42aa192d21a1348f48f53c1cdbfe485409ac17e
root <root@ip-10-0-0-41.ec2.internal>
2015-03-23 15:57:24 +0000

    updating minion/master create/update functions

--- phoenix/infrastructure/s3/minion/scripts/instance_manager.py
+++ phoenix/infrastructure/s3/minion/scripts/instance_manager.py
@@ -7,67 +7,78 @@ import key_deleter
 import time
 import helper
 
-POLL_CYCLE = 60 # seconds to sleep between polling dynamodb for list of active salt masters
+POLL_CYCLE = 31 # seconds to sleep between polling dynamodb for list of active salt masters
 MASTERS_TABLE_NAME = "masters"
 MINIONS_TABLE_NAME = "minions"
 MASTER_QUEUE = "SqsMaster"
 MINION_QUEUE = "SqsMinion"
 
 
-def create_master(instanceid, ipaddr, active):
+def create_master(region, instanceid, ipaddr, active):
   '''if an item exists in the masters table for this server, update it, otherwise create one'''
-  ### Neds to be updated
   result = False
   try:
+    ddb.set_region(region)
     a = ddb.MasterInstance()
-    masters = a.scan()
-    for m in masters:
-      if m.active == True:
-         master_ipaddrs.append(str(m.ipaddr))
+    a.instanceid = instanceid
+    a.ipaddr = ipaddr
+    a.active = active
+    a.save()
   except Exception, e:
     raise
   return result
 
-def update_master(instanceid, ipaddr, active):
-  '''if an item exists in the masters table for this server, update it, otherwise create one'''
-  ### Neds to be updated
+def update_master(region, instanceid, ipaddr, active):
+  '''if an item exists in the masters table for this server, update it, otherwise return False'''
   result = False
   try:
-    a = ddb.MasterInstance()
-    masters = a.scan()
-    for m in masters:
-      if m.active == True:
-         master_ipaddrs.append(str(m.ipaddr))
+    ddb.set_region(region)
+    a = ddb.MasterInstance.get(instanceid)
+    a.instanceid = instanceid
+    a.ipaddr = ipaddr
+    a.active = active
+    result = a.save()
   except Exception, e:
-    raise
+    pass
   return result
 
-def create_minion(instanceid, ipaddr, active):
-  '''if an item exists in the masters table for this server, update it, otherwise create one'''
-  ### Neds to be updated
+def create_minion(region, instanceid, modified=None, highstate_ran=None, highstate_runner=None, status=None):
+  '''create a minion in the dynamodb table '''
   result = False
   try:
-    a = ddb.MasterInstance()
-    masters = a.scan()
-    for m in masters:
-      if m.active == True:
-         master_ipaddrs.append(str(m.ipaddr))
+    ddb.set_region(region)
+    a = ddb.MinionInstance()
+    a.instanceid = instanceid
+    if modified:
+      a.modified = modified
+    if highstate_ran:
+      a.highstate_ran = highstate_ran
+    if highstate_runner:
+      a.highstate_runner = highstate_runner
+    if status:
+      a.status = status
+    result = a.save()
   except Exception, e:
-    raise
+    pass
   return result
 
-def update_minion(instanceid, ipaddr, active):
-  '''if an item exists in the masters table for this server, update it, otherwise create one'''
-  ### Neds to be updated
+def update_minion(region, instanceid, modified=None, highstate_ran=None, highstate_runner=None, status=None):
+  '''create a minion in the dynamodb table '''
   result = False
   try:
-    a = ddb.MasterInstance()
-    masters = a.scan()
-    for m in masters:
-      if m.active == True:
-         master_ipaddrs.append(str(m.ipaddr))
+    ddb.set_region(region)
+    a = ddb.MinionInstance.get(instanceid)
+    if modified:
+      a.modified = modified
+    if highstate_ran:
+      a.highstate_ran = highstate_ran
+    if highstate_runner:
+      a.highstate_runner = highstate_runner
+    if status:
+      a.status = status
+    result = a.save()
   except Exception, e:
-    raise
+    pass
   return result
 
 def remove_terminated_minion_keys():
@@ -78,22 +89,36 @@ def remove_terminated_minion_keys():
 
 def main():
   try:
+    info = sqs.load_ha_config_info()
+    queue_master = info['queuename_master']
+    region = info['region']
     instanceid = helper.get_instanceid()
     region = helper.get_region()
-    ipaddr = helper_get_private_ip()
+    ipaddr = helper.get_private_ip()
     ddb.set_region(region)
     ## set my ip address in the masters table
   except Exception, e:
     print "Unable to get necessary info, exiting: %s" % e
     sys.exit(1)
-  
+
+  # Attempt to update our master entry, and create it if we can't update it
+  try:  
+    result = update_master(region, instanceid, ipaddr, active=True)
+    if not result:
+      create_master(region, instanceid, ipaddr, active=True)
+  except Exception, e:
+    raise  
+ 
   while True:
     ## poll masters sqs queue, update masters table, and minions table
     try:
       sqs_conn = sqs.get_connection(region)
-      num_sqs_messages = sqs.get_queue_length(sqs_conn, MASTER_QUEUE)
+      num_sqs_messages = sqs.get_queue_length(sqs_conn, queue_master)
       for m in range(num_sqs_messages):
-        message = sqs.get_a_message(sqs_conn, queue)
+        message = sqs.get_a_message(sqs_conn, queue_master)
+        print message
+        print "----------------------------------------"
+        print
         # if launch, add to masters, add to minions
         # if terminate, update masters, update minions
     except Exception, e:
@@ -101,6 +126,7 @@ def main():
       raise
 
     ## poll minions sqs queue, update minions table
+    '''
     try:
       sqs_conn = sqs.get_connection(region)
       num_sqs_messages = sqs.get_queue_length(sqs_conn, MINION_QUEUE)
@@ -113,7 +139,15 @@ def main():
       raise
     ## get a list of all minions which are terminated
     ## and remove keys from self salt.Key 
+    '''
     time.sleep(POLL_CYCLE)
+
+if __name__ == "__main__":
+  try:
+    main()
+  except Exception, e:
+    print "Exception occurred: %s" % e
+
 
 '''
  - on loop e.g. once a minute
