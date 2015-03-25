c0f73f88a9d615f80b0db5e02f8173174692a69b
root <root@ip-10-0-0-41.ec2.internal>
2015-03-25 20:21:23 +0000

    close to finishing instance manager

--- phoenix/infrastructure/s3/minion/scripts/instance_manager.py
+++ phoenix/infrastructure/s3/minion/scripts/instance_manager.py
@@ -7,36 +7,41 @@ import key_deleter
 import time
 import helper
 
-POLL_CYCLE = 31 # seconds to sleep between polling dynamodb for list of active salt masters
+POLL_CYCLE = 15 # seconds to sleep between polling dynamodb for list of active salt masters
 MASTERS_TABLE_NAME = "masters"
 MINIONS_TABLE_NAME = "minions"
 MASTER_QUEUE = "SqsMaster"
 MINION_QUEUE = "SqsMinion"
 
 
-def create_master(region, instanceid, ipaddr, active):
+def create_master(region, instanceid, ipaddr=None, active=None):
   '''if an item exists in the masters table for this server, update it, otherwise create one'''
   result = False
   try:
     ddb.set_region(region)
     a = ddb.MasterInstance()
     a.instanceid = instanceid
-    a.ipaddr = ipaddr
-    a.active = active
+    if ipaddr:
+      a.ipaddr = ipaddr
+    if active:
+      a.active = active
     a.save()
   except Exception, e:
     raise
   return result
 
-def update_master(region, instanceid, ipaddr, active):
+def update_master(region=None, instanceid=None, ipaddr=None, active=None):
   '''if an item exists in the masters table for this server, update it, otherwise return False'''
   result = False
   try:
     ddb.set_region(region)
     a = ddb.MasterInstance.get(instanceid)
-    a.instanceid = instanceid
-    a.ipaddr = ipaddr
-    a.active = active
+    if instanceid:
+      a.instanceid = instanceid
+    if ipaddr:
+      a.ipaddr = ipaddr
+    if active:
+      a.active = active
     result = a.save()
   except Exception, e:
     pass
@@ -81,21 +86,47 @@ def update_minion(region, instanceid, modified=None, highstate_ran=None, highsta
     pass
   return result
 
-def remove_terminated_minion_keys():
-  '''Get list of terminated minions from minions table
-     if an instanceid/minionid is in the local salt masters keys,
-     delete the key'''
-  pass
+def get_terminated_minions(region):
+  '''Scan the dynamodb table for all minions
+     Return a list of those that have status not set to terminated'''
+  '''retrieve and return the list of active salt masters from dynamodb'''
+  instanceid_list = []
+  ddb.set_region(region)
+  try:
+    a = ddb.MinionInstance()
+    minions = a.scan()
+    for m in minions:
+      if m.status == 'terminated':
+         instanceid_list.append(str(m.instanceid))
+  except Exception, e:
+    raise
+  return instanceid_list
 
+def remove_terminated_minions(instance_ids):
+  '''Given a list of instance id, remove their key from the local salt master'''
+  result = True
+  try:
+    if instance_ids:
+      for i in instance_ids:
+        key_deleter.delete_key(i)
+  except Exception, e:
+    result = False
+    print "Error deleting key: %s" % e
+    pass
+  return result
+
+  
 def main():
   try:
     info = sqs.load_ha_config_info()
     queue_master = info['queuename_master']
+    queue_minion = info['queuename_minion']
     region = info['region']
     instanceid = helper.get_instanceid()
     region = helper.get_region()
     ipaddr = helper.get_private_ip()
     ddb.set_region(region)
+    sqs_conn = sqs.get_connection(region)
     ## set my ip address in the masters table
   except Exception, e:
     print "Unable to get necessary info, exiting: %s" % e
@@ -108,38 +139,66 @@ def main():
       create_master(region, instanceid, ipaddr, active=True)
   except Exception, e:
     raise  
- 
+
+  pass_no = 0 
   while True:
     ## poll masters sqs queue, update masters table, and minions table
+    pass_no += 1
+    print "pass # %s" % pass_no
     try:
-      sqs_conn = sqs.get_connection(region)
       num_sqs_messages = sqs.get_queue_length(sqs_conn, queue_master)
+      print "Number of messages in master queue: %s" % num_sqs_messages
       for m in range(num_sqs_messages):
         message = sqs.get_a_message(sqs_conn, queue_master)
-        print message.__dict__
-        print "----------------------------------------"
-        print
-        # if launch, add to masters, add to minions
-        # if terminate, update masters, update minions
+        instanceid = msg.get_instance_id(message)
+        print "MASTER: instanceid = %s" % instanceid
+        action = msg.get_instance_action(message)
+        print "MASTER: action = %s" % instanceid
+        if "LAUNCH" in action:
+          create_master(region=region, instanceid=instanceid)
+          create_minion(region=region, instanceid=instanceid, modified=None, highstate_ran=False,
+                        highstate_runner=None, status="active")
+        elif "TERMINATE" in action:
+          update_master(region=region, instanceid=instanceid, active=False)
+          update_minion(region=region, instanceid=instanceid, modified=None, highstate_ran=None,
+                        highstate_runner=None, status="terminated")
+        # Removing the message from the queue so that it does not repopulate automatically
+        sqs.delete_a_message(sqs_conn, queue_master, message)
     except Exception, e:
       print "Problem with master queue management: %s" % e
       raise
 
     ## poll minions sqs queue, update minions table
-    '''
     try:
-      sqs_conn = sqs.get_connection(region)
-      num_sqs_messages = sqs.get_queue_length(sqs_conn, MINION_QUEUE)
+      num_sqs_messages = sqs.get_queue_length(sqs_conn, queue_minion)
+      print "Number of messages in minion queue: %s" % num_sqs_messages
       for m in range(num_sqs_messages):
-        message = sqs.get_a_message(sqs_conn, queue)
-        # if launch, add to minions
-        # if terminate, update minions
+        message = sqs.get_a_message(sqs_conn, queue_minion)
+        instanceid = msg.get_instance_id(message)
+        print "MINION: instanceid = %s" % instanceid
+        action = msg.get_instance_action(message)
+        print "MINION: action = %s" % action
+        if "LAUNCH" in action:
+          create_minion(region=region, instanceid=instanceid, modified=None, highstate_ran=False,
+                        highstate_runner=None, status="active")
+        elif "TERMINATE" in action:
+          update_minion(region=region, instanceid=instanceid, modified=None, highstate_ran=None,
+                        highstate_runner=None, status="terminated")
+        # Removing the message from the queue so that it does not repopulate automatically
+        sqs.delete_a_message(sqs_conn, queue_minion, message)
     except Exception, e:
       print "Problem with minion queue management: %s" % e
       raise
+
     ## get a list of all minions which are terminated
-    ## and remove keys from self salt.Key 
-    '''
+    try:
+      result = False
+      instance_ids = get_terminated_minions(region)
+      result = remove_terminated_minions(instance_ids)
+    except Exception, e:
+      print "Error occurred: %s" % e
+      raise
+    
     time.sleep(POLL_CYCLE)
 
 if __name__ == "__main__":
@@ -147,17 +206,3 @@ if __name__ == "__main__":
     main()
   except Exception, e:
     print "Exception occurred: %s" % e
-
-
-'''
- - on loop e.g. once a minute
- - if salt-master is 'active', updates its 'active' state to true in dynamodb:masters table
- - polls master sqs queue
-   - update dynamodb:minions table accordingly (add / delete)
-     - get a list of all status==terminated minions, and ensure that their keys are not in salt.key
-   - update dynamodb:masters table accordingly (but set active to false)
- - polls minion sqs queue
-   - update dynamodb:minions table accordingly (add / delete)
- - this is how the ip address of a master gets updated in the masters table - updates its own ip address
- - it also updates the active to true after a highstate has been run on it
-'''
--- phoenix/infrastructure/s3/minion/scripts/msg.py
+++ phoenix/infrastructure/s3/minion/scripts/msg.py
@@ -30,13 +30,20 @@ def get_instance_id(message):
 def get_instance_action(message):
     try:
       bodydict = {}
+      actual_event = None
       text = message.get_body()
       mydict = json.loads(text)
       messagebody = mydict['Message']
       bodydict = json.loads(messagebody)
+      event = bodydict.get('Event', None)
+      # autoscaling:EC2_INSTANCE_LAUNCH
+      if event:
+        actual_event = event.split(":")[-1]
     except Exception, e:
       log.error("Error: %s" % e)
-    return bodydict.get('Event', None)
+      pass
+    #return bodydict.get('Event', None)
+    return actual_event
 
 if __name__ == "__main__":
   filename = sys.argv[1]
