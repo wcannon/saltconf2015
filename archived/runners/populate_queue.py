7300c6c032761850088a9af255ea969acdcb57db
root <root@ip-10-0-0-41.ec2.internal>
2015-03-25 21:51:20 +0000

    queue populater edited for better testing, fixing bugs in instance_manager

--- archived/runners/populate_queue.py
+++ archived/runners/populate_queue.py
@@ -3,11 +3,12 @@ import boto.sqs
 import boto.sqs.message
 
 region = 'us-east-1'
-queue = 'SqsMinion'
+#queue = 'SqsMinion'
+queue = 'SqsMaster'
 
 
 msg1 = open("instance_launch_msg.txt", "r").read()
-msg2 = open("instance_termination_msg.txt", "r").read()
+msg2 = open("instance_launch_msg2.txt", "r").read()
 
 conn = boto.sqs.connect_to_region(region)
 print "conn = %s" % conn
@@ -15,7 +16,7 @@ myq = conn.get_queue(queue)
 print "myq = %s" % myq
 my_messages = [msg1,msg2]
 for m in my_messages:
-  m =  boto.sqs.message.Message()
-  m.set_body(msg1)
-  myq.write(m)
+  mymessage =  boto.sqs.message.Message()
+  mymessage.set_body(m)
+  myq.write(mymessage)
 
--- phoenix/infrastructure/s3/minion/scripts/instance_manager.py
+++ phoenix/infrastructure/s3/minion/scripts/instance_manager.py
@@ -6,6 +6,7 @@ import sqs
 import key_deleter
 import time
 import helper
+import logging
 
 POLL_CYCLE = 15 # seconds to sleep between polling dynamodb for list of active salt masters
 MASTERS_TABLE_NAME = "masters"
@@ -26,7 +27,10 @@ def create_master(region, instanceid, ipaddr=None, active=None):
     if active:
       a.active = active
     a.save()
+    result = True
   except Exception, e:
+    print "In create_master()"
+    print "Exception raised: %s" % e
     raise
   return result
 
@@ -42,29 +46,27 @@ def update_master(region=None, instanceid=None, ipaddr=None, active=None):
       a.ipaddr = ipaddr
     if active:
       a.active = active
-    result = a.save()
+    a.save()
+    result = True
   except Exception, e:
-    pass
+    print "In update_master()"
+    print "Exception raised: %s" % e
+    raise
   return result
 
-def create_minion(region, instanceid, modified=None, highstate_ran=None, highstate_runner=None, status=None):
+def create_minion(region, instanceid):
   '''create a minion in the dynamodb table '''
   result = False
   try:
     ddb.set_region(region)
     a = ddb.MinionInstance()
     a.instanceid = instanceid
-    if modified:
-      a.modified = modified
-    if highstate_ran:
-      a.highstate_ran = highstate_ran
-    if highstate_runner:
-      a.highstate_runner = highstate_runner
-    if status:
-      a.status = status
-    result = a.save()
+    a.save()
+    result = True
   except Exception, e:
-    pass
+    #debug.log("problem saving minion entry: %e" % e)
+    print "problem saving minion entry: %e" % e
+    raise
   return result
 
 def update_minion(region, instanceid, modified=None, highstate_ran=None, highstate_runner=None, status=None):
@@ -81,9 +83,12 @@ def update_minion(region, instanceid, modified=None, highstate_ran=None, highsta
       a.highstate_runner = highstate_runner
     if status:
       a.status = status
-    result = a.save()
+    a.save()
+    result = True
   except Exception, e:
-    pass
+    print "In update_minion()"
+    print "Exception raised: %s" % e
+    raise
   return result
 
 def get_terminated_minions(region):
@@ -99,6 +104,8 @@ def get_terminated_minions(region):
       if m.status == 'terminated':
          instanceid_list.append(str(m.instanceid))
   except Exception, e:
+    print "In get_terminated_minions()"
+    print "Exception raised: %s" % e
     raise
   return instanceid_list
 
@@ -112,11 +119,19 @@ def remove_terminated_minions(instance_ids):
   except Exception, e:
     result = False
     print "Error deleting key: %s" % e
-    pass
+    raise
   return result
 
   
 def main():
+  logger = logging.getLogger('instance_manager')
+  logger.setLevel(logging.DEBUG)
+  fh = logging.FileHandler('instance_manager.log')
+  fh.setLevel(logging.DEBUG)
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  fh.setFormatter(formatter)
+  logger.addHandler(fh)
+
   try:
     info = sqs.load_ha_config_info()
     queue_master = info['queuename_master']
@@ -127,17 +142,21 @@ def main():
     ipaddr = helper.get_private_ip()
     ddb.set_region(region)
     sqs_conn = sqs.get_connection(region)
+    logger.debug("in main() function - retrieved info, queues, region, self instanceid & etc.")
     ## set my ip address in the masters table
   except Exception, e:
-    print "Unable to get necessary info, exiting: %s" % e
+    logger.debug("Unable to get necessary info, exiting: %s" % e)
     sys.exit(1)
 
   # Attempt to update our master entry, and create it if we can't update it
   try:  
     result = update_master(region, instanceid, ipaddr, active=True)
+    logger.debug("updating master info (ipaddr and etc)")
     if not result:
       create_master(region, instanceid, ipaddr, active=True)
+      logger.debug("master ref not found, creating one for self")
   except Exception, e:
+    logger.debug("Exception: %s" % e)
     raise  
 
   pass_no = 0 
@@ -147,25 +166,35 @@ def main():
     print "pass # %s" % pass_no
     try:
       num_sqs_messages = sqs.get_queue_length(sqs_conn, queue_master)
+      logger.debug("number of sqs messages for master = %s" % num_sqs_messages)
       print "Number of messages in master queue: %s" % num_sqs_messages
       for m in range(num_sqs_messages):
         message = sqs.get_a_message(sqs_conn, queue_master)
         instanceid = msg.get_instance_id(message)
         print "MASTER: instanceid = %s" % instanceid
         action = msg.get_instance_action(message)
-        print "MASTER: action = %s" % instanceid
+        print "MASTER: action = %s" % action
         if "LAUNCH" in action:
+          print "creating master"
           create_master(region=region, instanceid=instanceid)
-          create_minion(region=region, instanceid=instanceid, modified=None, highstate_ran=False,
-                        highstate_runner=None, status="active")
+          logger.debug("creating master: region: %s, instanceid: %s" % (region,instanceid) )
+          print "creating minion entry for master"
+          create_minion(region=region, instanceid=instanceid)
+          logger.debug("creating minion entry for master: region: %s, instanceid: %s" % (region,instanceid) )
         elif "TERMINATE" in action:
+          print "updating master"
           update_master(region=region, instanceid=instanceid, active=False)
+          logger.debug("updating master info : instanceid: %s" % (region,instanceid) )
+          print "updating minion entry for master"
           update_minion(region=region, instanceid=instanceid, modified=None, highstate_ran=None,
                         highstate_runner=None, status="terminated")
+          logger.debug("updating minion entry for master : instanceid: %s" % (region,instanceid) )
         # Removing the message from the queue so that it does not repopulate automatically
+        print "removing message from sqs - b/c we dealt with it"
         sqs.delete_a_message(sqs_conn, queue_master, message)
     except Exception, e:
-      print "Problem with master queue management: %s" % e
+      #print "Problem with master queue management: %s" % e
+      logger.debug("Master queue mgmt -- Exception: %s" % e)
       raise
 
     ## poll minions sqs queue, update minions table
@@ -179,15 +208,15 @@ def main():
         action = msg.get_instance_action(message)
         print "MINION: action = %s" % action
         if "LAUNCH" in action:
-          create_minion(region=region, instanceid=instanceid, modified=None, highstate_ran=False,
-                        highstate_runner=None, status="active")
+          create_minion(region=region, instanceid=instanceid)
         elif "TERMINATE" in action:
           update_minion(region=region, instanceid=instanceid, modified=None, highstate_ran=None,
                         highstate_runner=None, status="terminated")
         # Removing the message from the queue so that it does not repopulate automatically
         sqs.delete_a_message(sqs_conn, queue_minion, message)
     except Exception, e:
-      print "Problem with minion queue management: %s" % e
+      #print "Problem with minion queue management: %s" % e
+      logger.debug("Minion queue mgmt -- Exception: %s" % e)
       raise
 
     ## get a list of all minions which are terminated
@@ -196,7 +225,7 @@ def main():
       instance_ids = get_terminated_minions(region)
       result = remove_terminated_minions(instance_ids)
     except Exception, e:
-      print "Error occurred: %s" % e
+      logger.debug("Exception: %s" % e)
       raise
     
     time.sleep(POLL_CYCLE)
@@ -205,4 +234,4 @@ if __name__ == "__main__":
   try:
     main()
   except Exception, e:
-    print "Exception occurred: %s" % e
+    print "Exception: %s" % e
